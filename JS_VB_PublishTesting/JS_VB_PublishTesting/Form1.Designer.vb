66f350c3251da2f0f7755dc456643a632af90ce0
jselinger <jsel2008@witxp.com>
2014-04-05 17:59:37 -0400

    Testing first change tracking

--- JS_VB_PublishTesting/JS_VB_PublishTesting/Form1.Designer.vb
+++ JS_VB_PublishTesting/JS_VB_PublishTesting/Form1.Designer.vb
@@ -30,9 +30,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(97, 88)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.Size = New System.Drawing.Size(65, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Hellow World!"
+        Me.Label1.Text = "Hello World!"
         '
         'Form1
         '
